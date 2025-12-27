a1297a88b3a1e2fa5f7b0307f857dd4fb7c0d239
zjr231 <1920469286@qq.com>
2025-12-27 15:19:53 +0800

    添加了读文件功能，但是西湖景区的地图还要优化一下，数据不对，然后每个景点的相对位置也有点问题

--- scenicspot.cpp
+++
@@ -1,7 +0,0 @@
-#include "scenicspot.h"
-
-ScenicSpot::ScenicSpot() : m_id(0) {}
-
-ScenicSpot::ScenicSpot(int id, const QString& name, const QString& description)
-    : m_id(id), m_name(name), m_description(description) {}
-
